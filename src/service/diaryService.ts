16926e29e9a2b5f07488d010734494b049e4bf4a
Jovany Vergara <jovany@makingdevs.com>
2023-03-31 15:41:30 -0600

    [main] create new service

--- src/service/diaryService.ts
+++ src/service/diaryService.ts
@@ -1,8 +1,10 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => diaries
 
 export const addEntry = (): undefined => undefined
