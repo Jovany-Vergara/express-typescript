32b37e8883a511bda9dce22f3b41a562a23f664d
Jovany Vergara <jovany@makingdevs.com>
2023-04-05 18:00:40 -0600

    [main] create fuction in router

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -7,6 +7,14 @@ router.get('/', (_req, res) => {
   res.send(diaryService.getEntriesWithoutSensitiveInfo())
 })
 
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(+req.params.id)
+
+  return (diary != null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
+
 router.post('/', (_req, res) => {
   res.send('Saving a diary')
 })
