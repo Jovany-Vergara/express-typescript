f73880910790d4be645dffdf6b62ae09e0cd017f
Jovany Vergara <jovany@makingdevs.com>
2023-04-05 18:00:19 -0600

    [main] create new service and method

--- src/service/diaryService.ts
+++ src/service/diaryService.ts
@@ -5,6 +5,11 @@ const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
+export const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+  return entry
+}
+
 export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => {
     return {
